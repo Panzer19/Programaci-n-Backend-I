6066b74a4e69bf17ff5b803a1930d3ebb6e9ddaa
Panzer19 <102765748+Panzer19@users.noreply.github.com>
2024-10-29 20:25:38 -0300

    MK1/PRIMERA ENTREGA

--- src/managers/cart.manager.js
+++ src/managers/cart.manager.js
@@ -1,80 +1,77 @@
-
-
-import fs from "node:fs";
-import { v4 as uuidv4 } from "uuid";
+import fs from "node:fs"
+import { v4 as uuidv4 } from "uuid"
 import ProductManager from "./product.manager.js"
 
 export default class CartManager {
-
   constructor(path) {
-    this.path = path;
+    this.path = path
   }
 
   async getCarts() {
     try {
       if (fs.existsSync(this.path)) {
-        const carts = await fs.promises.readFile(this.path, "utf-8");
-        return JSON.parse(carts);
-      } else return [];
+        const carts = await fs.promises.readFile(this.path, "utf-8")
+        return JSON.parse(carts)
+      } else return []
     } catch (error) {
-      throw new Error(error.message);
+      throw new Error(error.message)
     }
   }
 
   async createCart() {
     try {
-      const newCart = { id: uuidv4(), products: [] };
-      const carts = await this.getCarts();
-      carts.push(newCart);
-      await fs.promises.writeFile(this.path, JSON.stringify(carts));
-      return newCart;
+      const newCart = { id: uuidv4(), products: [] }
+      const carts = await this.getCarts()
+      carts.push(newCart)
+      await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2))
+      return newCart
     } catch (error) {
-      throw new Error(error.message);
+      throw new Error(error.message)
     }
   }
 
   async getCartById(id) {
     try {
-      const carts = await this.getCarts();
-      const cart = carts.find(cart => cart.id === id);
-      if (!cart) throw new Error("Cart not found");
-      return cart;
+      const carts = await this.getCarts()
+      const cart = carts.find(cart => cart.id === id)
+      if (!cart) throw new Error("Cart not found")
+      return cart
     } catch (error) {
-      throw new Error(error.message);
+      throw new Error(error.message)
     }
   }
 
-  async addoUpdate(cid, pid) {
+  async addOrUpdateProductInCart(cid, pid) {
     try {
-      const carts = await this.getCarts();
-      const cart = carts.find(cart => cart.id === cid);
-      if (!cart) throw new Error("Cart not found");
+      const carts = await this.getCarts()
+      const cart = carts.find(cart => cart.id === cid)
+      if (!cart) throw new Error("Cart not found")
 
-      const productExists = await ProductManager.exists(pid);
-      if (!productExists) throw new Error("Product not found");
+      const productExists = await ProductManager.exists(pid)
+      if (!productExists) throw new Error("Product not found")
 
-      const productInCart = cart.products.find(product => product.id === pid);
+      const productInCart = cart.products.find(product => product.product === pid)
       if (productInCart) {
-        productInCart.quantity++;
+        productInCart.quantity++
       } else {
-        cart.products.push({ id: pid, quantity: 1 });
+        cart.products.push({ product: pid, quantity: 1 })
       }
 
-      await fs.promises.writeFile(this.path, JSON.stringify(carts));
-      return cart;
+      await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2))
+      return cart
     } catch (error) {
-      throw new Error(error.message);
+      throw new Error(error.message)
     }
   }
 
   async deleteCartById(id) {
     try {
-      const carts = await this.getCarts();
-      const newArray = carts.filter(cart => cart.id !== id);
-      if (newArray.length === carts.length) throw new Error("Cart not found");
-      await fs.promises.writeFile(this.path, JSON.stringify(newArray));
+      const carts = await this.getCarts()
+      const newArray = carts.filter(cart => cart.id !== id)
+      if (newArray.length === carts.length) throw new Error("Cart not found")
+      await fs.promises.writeFile(this.path, JSON.stringify(newArray, null, 2))
     } catch (error) {
-      throw new Error(error.message);
+      throw new Error(error.message)
     }
   }
 }
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,51 +1,36 @@
+import { Router } from "express"
+import CartManager from "../managers/cart.manager.js"
+const cartManager = new CartManager("./carts.json")
 
-import { Router } from "express";
-import CartManager from "../managers/cart.manager.js";
-const cartManager = new CartManager("./carts.json");
-
-const router = Router();
-
+const router = Router()
 
 router.post("/", async (req, res) => {
   try {
-    const cart = await cartManager.createCart();
-    res.status(201).json(cart);
+    const cart = await cartManager.createCart()
+    res.status(201).json(cart)
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-});
-
+})
 
 router.get("/:cid", async (req, res) => {
   try {
-    const { cid } = req.params;
-    const cart = await cartManager.getCartById(cid);
-    res.status(200).json(cart);
+    const { cid } = req.params
+    const cart = await cartManager.getCartById(cid)
+    res.status(200).json(cart.products)
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error.message })
   }
-});
-
+})
 
 router.post("/:cid/product/:pid", async (req, res) => {
   try {
-    const { cid, pid } = req.params;
-    const cart = await cartManager.addoUpdate(cid, pid);
-    res.status(200).json(cart);
+    const { cid, pid } = req.params
+    const cart = await cartManager.addOrUpdateProductInCart(cid, pid)
+    res.status(200).json(cart)
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error.message })
   }
-});
+})
 
-
-router.delete("/:cid", async (req, res) => {
-  try {
-    const { cid } = req.params;
-    await cartManager.deleteCartById(cid);
-    res.status(200).json({ message: `Cart id: ${cid} deleted successfully` });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-});
-
-export default router;
+export default router
